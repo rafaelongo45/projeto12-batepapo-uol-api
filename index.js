c3baf98fd1ee40fd5c2d89bceae280e3c2e9d1a3
Rafael <rafaelongo45@gmail.com>
2022-04-28 20:05:19 -0300

    Validação na rota participants feita com a biblioteca joi. Não pode ter 2 usuários com o mesmo nome.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import express from "express"
+import joi from "joi";
 import cors from "cors"
 import chalk from "chalk";
-import { MongoClient } from "mongodb";
 import dayjs from "dayjs";
 import dotenv from "dotenv";
+import express from "express"
+import { MongoClient, ObjectId } from "mongodb";
 
 dotenv.config();
 const mongoClient = new MongoClient(process.env.MONGO_URI);
@@ -12,13 +13,43 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/participants", async (req, res) => { //Validação com JOI do nome do usuario. Não pode ser uma string vazia e também não pode ser igual a de um nome ja existente no banco de dados!
+app.post("/participants", async (req, res) => {
   const {name} = req.body;
+  const schema = joi.object({
+      username: joi.string()
+      .required()
+  });
+
+  const {error, value} = schema.validate({username: name});
+  
+  if(error !== undefined){
+    console.log(chalk.bold.red(error.details[0].message))
+    res.sendStatus(422);
+    return
+  }
 
   try{
     await mongoClient.connect();
     const dbUol = mongoClient.db("batepapouol");
     const usersCollection = dbUol.collection("users");
+    
+    const usersArray = await usersCollection.find({}).toArray();
+    const usernamesArray  = usersArray.map(username => username.name)
+
+    const alreadyLoggedIn = usernamesArray.find((user) => {
+      if(user === name){
+        return true;
+      }else{
+        return false
+      }
+    });
+
+    if(alreadyLoggedIn){
+      res.sendStatus(409);
+      console.log("Usuário já cadastrado")
+      return
+    }
+
     await usersCollection.insertOne({name: name, lastStatus: Date.now()});
     const messagesCollection = dbUol.collection("messages");
     await messagesCollection.insertOne({from:name, to:'Todos', text: 'entra na sala...', type: 'status', time:dayjs().format('HH:mm:ss')});
